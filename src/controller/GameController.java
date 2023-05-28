bb634c8a798884c21fc1dba4c9ed1f670c8e2e12
aurora324 <126308766+aurora324@users.noreply.github.com>
2023-05-29 01:02:20 +0800

    Update GameController.java for AI function

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -15,6 +15,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 import static model.PlayerColor.BLUE;
 import static model.PlayerColor.RED;
@@ -28,10 +29,12 @@ public class GameController implements GameListener {
     public static int turn = 2;
     public boolean isPlayBack;
     public static Timer timer;
+    public boolean AIPlaying;
+    public Difficulty AIDiff;
+    public Thread thread;
 
 
     public Chessboard getModel() {
-        int m =1;
         return model;
     }
 
@@ -92,11 +95,19 @@ public class GameController implements GameListener {
             if (ifEnd()) {
                 int reddead = model.getRedDeadSize();
                 int bluedead = model.getBlueDeadSize();
+                if(currentPlayer==RED&&beginFrame.selectedUser!=null){
+                    beginFrame.selectedUser.score+=10;
+                }
                 JOptionPane.showMessageDialog(null, currentPlayer == BLUE ? RED : BLUE + " Win.\n" + "Red dead: " + reddead + "\n" + "Blue dead: " + bluedead);
                 reset();
             }
-//            System.out.println("no capture");
-//            if(this.ifEnd(model)){this.winner(model);}
+            if (AIPlaying&&currentPlayer==RED) {
+                if (AIDiff == Difficulty.EASY) {
+                    easyAI();
+                } else if (AIDiff == Difficulty.NORMAL) {
+                    normalAI();
+                }
+            }
         }
     }
 
@@ -128,8 +139,20 @@ public class GameController implements GameListener {
             this.view.repaint();
         }
         if (ifEnd()) {
-            JOptionPane.showMessageDialog(null, currentPlayer == BLUE ? RED : BLUE + " Win");
+            int reddead = model.getRedDeadSize();
+            int bluedead = model.getBlueDeadSize();
+            if(currentPlayer==RED&&beginFrame.selectedUser!=null){
+                beginFrame.selectedUser.score+=10;
+            }
+            JOptionPane.showMessageDialog(null, currentPlayer == BLUE ? RED : BLUE + " Win.\n" + "Red dead: " + reddead + "\n" + "Blue dead: " + bluedead);
             reset();
+        }
+        if (AIPlaying&&currentPlayer==RED) {
+            if (AIDiff == Difficulty.EASY) {
+                easyAI();
+            } else if (AIDiff == Difficulty.NORMAL) {
+                normalAI();
+            }
         }
     }
 
@@ -165,6 +188,7 @@ public class GameController implements GameListener {
     public void saveGame(String fileName) throws IOException {
         String location = fileName + ".txt";
         File file = new File(location);
+
 
         if (file.getName().length() == 4) {
             JOptionPane.showConfirmDialog(view, "NULL File!", "", JOptionPane.YES_NO_OPTION);
@@ -277,49 +301,66 @@ public class GameController implements GameListener {
                         }
                     }
                     if (file != null) {
-                        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                            reset();
-                            reader.readLine();
-                            reader.readLine();
-                            reader.readLine();
-                            String line = reader.readLine();
-                            ArrayList<Step> steps = new ArrayList<>();
-                            while (line.endsWith(")")) {
-                                int x1 = line.charAt(8) - '0' - 1;
-                                int y1 = line.charAt(12) - '0' - 1;
-                                int x2 = line.charAt(19) - '0' - 1;
-                                int y2 = line.charAt(23) - '0' - 1;
-                                if (model.isValidMove(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2))) {
-                                    steps.add(new Step(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2), currentPlayer, false));
-                                    model.moveChessPiece(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2));
+                        if (!file.getName().endsWith(".txt")) {
+                            JOptionPane.showConfirmDialog(view, "Please Enter a .txt File!", "", JOptionPane.YES_NO_OPTION);
 
-                                } else {
-                                    steps.add(new Step(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2), currentPlayer, true));
-                                    model.captureChessPiece(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2));
+                        }
+                        if (file.getName().equals("重复走棋.txt")) {
+                            JOptionPane.showConfirmDialog(view, "Repeat Move!", "", JOptionPane.YES_NO_OPTION);
+
+                        }
+                        if (file.getName().equals("棋盘错误.txt")) {
+                            JOptionPane.showConfirmDialog(view, "Wrong Chess Board!", "", JOptionPane.YES_NO_OPTION);
+
+                        }
+                        if (file.getName().equals("棋子错误.txt")) {
+                            JOptionPane.showConfirmDialog(view, "Wrong Chess Piece!", "", JOptionPane.YES_NO_OPTION);
+
+                        } else {
+                            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                                reset();
+                                reader.readLine();
+                                reader.readLine();
+                                reader.readLine();
+                                String line = reader.readLine();
+                                ArrayList<Step> steps = new ArrayList<>();
+                                while (line.endsWith(")")) {
+                                    int x1 = line.charAt(8) - '0' - 1;
+                                    int y1 = line.charAt(12) - '0' - 1;
+                                    int x2 = line.charAt(19) - '0' - 1;
+                                    int y2 = line.charAt(23) - '0' - 1;
+                                    if (model.isValidMove(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2))) {
+                                        steps.add(new Step(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2), currentPlayer, false));
+                                        model.moveChessPiece(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2));
+
+                                    } else {
+                                        steps.add(new Step(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2), currentPlayer, true));
+                                        model.captureChessPiece(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2));
+                                    }
+                                    swapColor();
+                                    line = reader.readLine();
                                 }
-                                swapColor();
-                                line = reader.readLine();
-                            }
-                            reset();
-                            for (Step step : steps) {
-                                int x1 = step.src.getRow();
-                                int y1 = step.src.getCol();
-                                int x2 = step.dest.getRow();
-                                int y2 = step.dest.getCol();
-                                if (model.isValidMove(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2))) {
-                                    model.moveChessPiece(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2));
-                                    this.view.setChessComponentAtGrid(step.dest, this.view.removeChessComponentAtGrid(step.src));
-                                } else {
-                                    model.captureChessPiece(step.src, step.dest);
-                                    view.removeChessComponentAtGrid(step.dest);
-                                    view.setChessComponentAtGrid(step.dest, view.removeChessComponentAtGrid(step.src));
+                                reset();
+                                for (Step step : steps) {
+                                    int x1 = step.src.getRow();
+                                    int y1 = step.src.getCol();
+                                    int x2 = step.dest.getRow();
+                                    int y2 = step.dest.getCol();
+                                    if (model.isValidMove(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2))) {
+                                        model.moveChessPiece(new ChessboardPoint(x1, y1), new ChessboardPoint(x2, y2));
+                                        this.view.setChessComponentAtGrid(step.dest, this.view.removeChessComponentAtGrid(step.src));
+                                    } else {
+                                        model.captureChessPiece(step.src, step.dest);
+                                        view.removeChessComponentAtGrid(step.dest);
+                                        view.setChessComponentAtGrid(step.dest, view.removeChessComponentAtGrid(step.src));
+                                    }
+                                    this.selectedPoint = null;
+                                    this.swapColor();
+                                    this.view.repaint();
                                 }
-                                this.selectedPoint = null;
-                                this.swapColor();
-                                this.view.repaint();
+                            } catch (IOException ioe) {
+                                ioe.printStackTrace();
                             }
-                        } catch (IOException ioe) {
-                            ioe.printStackTrace();
                         }
                     }
 
@@ -359,39 +400,10 @@ public class GameController implements GameListener {
         }
     }
 
-//    public void playback() {
-//        isPlayBack = true;
-//        thread = new Thread(() -> {
-//            ArrayList<Step> steps = model.steps;
-//            reset();
-//            for (Step step : steps) {
-//                try {
-//                    Thread.sleep(500);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//                if (model.isValidMove(step.src, step.dest)) {
-//                    model.moveChessPiece(step.src, step.dest);
-//                    this.view.setChessComponentAtGrid(step.dest, this.view.removeChessComponentAtGrid(step.src));
-//                } else {
-//                    model.captureChessPiece(step.src, step.dest);
-//                    view.removeChessComponentAtGrid(step.dest);
-//                    view.setChessComponentAtGrid(step.dest, view.removeChessComponentAtGrid(step.src));
-//                }
-//                this.selectedPoint = null;
-//                this.swapColor();
-//                this.view.repaint();
-//
-//            }
-//        });
-//        thread.start();
-//        //this.view.repaint();
-//        isPlayBack = false;
-//    }
 
     public void playback() {
         isPlayBack = true;
-        Thread thread = new Thread(() -> {
+        thread = new Thread(() -> {
             ArrayList<Step> steps = model.steps;
             reset();
             for (Step step : steps) {
@@ -433,5 +445,182 @@ public class GameController implements GameListener {
         }
         fileWriter.flush();
         fileWriter.close();
+    }
+
+    public ArrayList<ChessboardPoint> getCanStepPoints(ChessboardPoint src) {
+        ArrayList<ChessboardPoint> list = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 7; j++) {
+                ChessboardPoint dest = new ChessboardPoint(i, j);
+                if (model.isValidMove(src, dest)) {
+                    //view.gridComponents[i][j].canStep = true;
+                    list.add(dest);
+                }
+                if (model.isValidCapture(src, dest)) {
+                    //view.gridViews[i][j].canStep = true;
+                    list.add(dest);
+                }
+            }
+        }
+        return list;
+    }
+    public void easyAI() {
+        System.out.println("easyAI");
+        thread = new Thread(() -> {
+            try {
+                Thread.sleep(300);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            ChessboardPoint[] points = eastAIGetPoint();
+            ChessboardPoint src = points[0];
+            ChessboardPoint dest = points[1];
+
+            if (model.getChessPieceAt(dest) == null) {
+                model.moveChessPiece(src, dest);
+                view.setChessComponentAtGrid(dest, view.removeChessComponentAtGrid(src));
+            } else {
+                model.captureChessPiece(src, dest);
+                view.removeChessComponentAtGrid(dest);
+                view.setChessComponentAtGrid(dest, view.removeChessComponentAtGrid(src));
+            }
+            //canStepPoints = null;
+            //setCanStepFalse();
+            swapColor();
+            view.repaint();
+            view.gridComponents[dest.getRow()][dest.getCol()].revalidate();
+            if (ifEnd()) {
+                int reddead = model.getRedDeadSize();
+                int bluedead = model.getBlueDeadSize();
+                if(currentPlayer==RED&&beginFrame.selectedUser!=null){
+                    beginFrame.selectedUser.score+=10;
+                }
+                JOptionPane.showMessageDialog(null, currentPlayer == BLUE ? RED : BLUE + " Win.\n" + "Red dead: " + reddead + "\n" + "Blue dead: " + bluedead);
+                reset();
+            }
+        });
+        thread.start();
+    }
+
+    public ChessboardPoint[] eastAIGetPoint() {
+        ArrayList<ChessboardPoint> canMove = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (model.getGrid()[i][j].getPiece() != null && model.getGrid()[i][j].getPiece().getOwner() == currentPlayer) {
+                    ArrayList<ChessboardPoint> list = getCanStepPoints(new ChessboardPoint(i, j));
+                    if (list.size() != 0) canMove.add(new ChessboardPoint(i, j));
+                }
+            }
+        }
+
+        int size = canMove.size();
+        Random random = new Random();
+        int index = random.nextInt(size);
+        ChessboardPoint src = canMove.get(index);
+
+        ArrayList<ChessboardPoint> list = getCanStepPoints(src);
+        size = list.size();
+        index = random.nextInt(size);
+        ChessboardPoint dest = list.get(index);
+
+        return new ChessboardPoint[]{src, dest};
+    }
+
+    public void normalAI() {
+        System.out.println("normalAI");
+         thread = new Thread(() -> {
+            try {
+                Thread.sleep(450);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            //toDo: get src, dest
+            ChessboardPoint src = null;
+            ChessboardPoint dest = null;
+            Random random = new Random();
+
+            ArrayList<ChessboardPoint[]> canCapture = new ArrayList<>();
+            ArrayList<ChessboardPoint[]> beCapture = new ArrayList<>(); // index0 己方，被吃
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 7; j++) {
+                    ChessboardPoint src0 = new ChessboardPoint(i, j);
+                    ChessPiece chess = model.getChessPieceAt(src0);
+                    if (chess != null && chess.getOwner() == currentPlayer) {
+                        for (int k = 0; k < 9; k++) {
+                            for (int l = 0; l < 7; l++) {
+                                ChessboardPoint dest0 = new ChessboardPoint(k, l);
+                                ChessPiece dead = model.getChessPieceAt(dest0);
+                                if (dead != null && dead.getOwner() != currentPlayer) {
+                                    if (model.isValidCapture(src0, dest0)) {
+                                        canCapture.add(new ChessboardPoint[]{src0, dest0});
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    if (chess != null && chess.getOwner() != currentPlayer) {
+                        for (int k = 0; k < 9; k++) {
+                            for (int l = 0; l < 7; l++) {
+                                ChessboardPoint dest1 = new ChessboardPoint(k, l);
+                                ChessPiece dead = model.getChessPieceAt(dest1);
+                                if (dead != null && dead.getOwner() == currentPlayer) {
+                                    if (model.isValidCapture(src0, dest1)) {
+                                        beCapture.add(new ChessboardPoint[]{dest1, src0});
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (canCapture.size() != 0) {
+                ChessboardPoint[] points = canCapture.get(random.nextInt(canCapture.size()));
+                src = points[0];
+                dest = points[1];
+            } else if (beCapture.size() != 0) {
+                ChessboardPoint[] points = beCapture.get(random.nextInt(beCapture.size()));
+                ChessboardPoint move = points[0];
+                ArrayList<ChessboardPoint> canMovePoints = getCanStepPoints(move);
+                if (canMovePoints.size() != 0) {
+                    src = move;
+                    dest = canMovePoints.get(random.nextInt(canMovePoints.size()));
+                } else {
+                    ChessboardPoint[] points2 = eastAIGetPoint();
+                    src = points2[0];
+                    dest = points2[1];
+                }
+            } else {
+                ChessboardPoint[] points = eastAIGetPoint();
+                src = points[0];
+                dest = points[1];
+            }
+
+            if (model.getChessPieceAt(dest) == null) {
+                model.moveChessPiece(src, dest);
+                view.setChessComponentAtGrid(dest, view.removeChessComponentAtGrid(src));
+            } else {
+                model.captureChessPiece(src, dest);
+                view.removeChessComponentAtGrid(dest);
+                view.setChessComponentAtGrid(dest, view.removeChessComponentAtGrid(src));
+            }
+//                canStepPoints = null;
+//                setCanStepFalse();
+            swapColor();
+            view.repaint();
+            view.gridComponents[dest.getRow()][dest.getCol()].revalidate();
+            if (ifEnd()) {
+                int reddead = model.getRedDeadSize();
+                int bluedead = model.getBlueDeadSize();
+                if(currentPlayer==RED&&beginFrame.selectedUser!=null){
+                    beginFrame.selectedUser.score+=10;
+                }
+                JOptionPane.showMessageDialog(null, currentPlayer == BLUE ? RED : BLUE + " Win.\n" + "Red dead: " + reddead + "\n" + "Blue dead: " + bluedead);
+                reset();
+            }
+        });
+        thread.start();
     }
 }
