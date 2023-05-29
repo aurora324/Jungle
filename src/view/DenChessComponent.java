3aaafdd26dc0f709c8f00c25f06c732403f9417d
qiuuuuv <124148157+qiuuuuv@users.noreply.github.com>
2023-05-30 00:13:56 +0800

    Update DenChessComponent.java

--- src/view/DenChessComponent.java
+++ src/view/DenChessComponent.java
@@ -5,21 +5,32 @@
 
 package view;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
-import javax.swing.JComponent;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import javax.swing.*;
+
 import model.PlayerColor;
 
 public class DenChessComponent extends JComponent {
     private PlayerColor owner;
-
-    public DenChessComponent(PlayerColor owner, int size) {
-        this.owner = owner;
-        this.setSize(size / 2, size / 2);
-        this.setLocation(0, 0);
-        this.setVisible(true);
+//    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        Graphics2D g2 = (Graphics2D)g;
+//        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//        g2.setColor(this.owner.getColor());
+//    }
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        ImageIcon pic = new ImageIcon("src/resources/bird-cage.png");
+        if (owner == PlayerColor.BLUE){
+            pic = new ImageIcon("src/resources/bird-cage (1).png");
+        }
+        Image image = pic.getImage();
+        pic = new ImageIcon(image.getScaledInstance(this.getWidth(), this.getWidth(),Image.SCALE_SMOOTH));
+        JLabel label = new JLabel(pic);
+        label.setSize(this.getWidth(), this.getWidth());
+        //bgLabel.setLocation(0, 0);
+        add(label);
+        }
     }
-}
+
