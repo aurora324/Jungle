8601d0581cdf138e206491d52659bd1aac06968d
aurora324 <126308766+aurora324@users.noreply.github.com>
2023-05-30 00:13:57 +0800

    Update Timer.java

--- src/model/Timer.java
+++ src/model/Timer.java
@@ -29,38 +29,9 @@ public class Timer extends Thread {
                         e.printStackTrace();
                     }
                 }
-                time = 45;
-
-//                if (b){
-//                    ChessboardPoint[] points = gameController.eastAIGetPoint();
-//                    ChessboardPoint src = points[0];
-//                    ChessboardPoint dest = points[1];
-//
-//                    if (gameController.model.getChessPieceAt(dest) == null){
-//                        gameController.model.moveChessPiece(src, dest);
-//                        gameController.view.setChessComponentAtGrid(dest, gameController.view.removeChessComponentAtGrid(src));
-//                    } else {
-//                        gameController.model.captureChessPiece(src, dest);
-//                        gameController.view.removeChessComponentAtGrid(dest);
-//                       gameController.view.setChessComponentAtGrid(dest, gameController.view.removeChessComponentAtGrid(src));
-//                    }
-//                    gameController.canStepPoints = null;
-//                    gameController.setCanStepFalse();
-//                    gameController.swapColor();
-//                    gameController.view.repaint();
-//                    gameController.view.gridComponents[dest.getRow()][dest.getCol()].revalidate();
-//                    gameController.checkWin();
-//                    if (gameController.winner != null){
-//                        gameController.doWin();
-//                        gameController.reset();
-//                    }
-//                } else {
-//                    gameController.swapColor();
-//                }
-
+                time = 60;
             }
         }
-
     }
 
     public Timer(GameController gameController){
